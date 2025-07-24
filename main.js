fb86e160cce4014855d608b3775b54aafdcf3349
BaCat68 <andreyka_l08@mail.ru>
2025-07-25 00:28:13 +0700

    Обновление 0.2

--- main.js
+++ main.js
@@ -1,26 +1,53 @@
-// const review_window_overlay = document.getElementById("review-window-overlay")
-// const review_window = document.getElementById("review-window")
+// let container = document.getElementById("all-music");
+// let children = container.children;
 
-// function showReview() {
-//     review_window_overlay.style.display = "block";
+
+
+// let index = Math.floor(Math.random() * children.length);
+
+// for(let i = 0; i < children.length; i++) {
+//     if(index != i) {
+//         children[i].style.display= "none";
+//     }
 // }
 
-// function hideReview() {
-//     review_window_overlay.style.display = "none";
-// }
 
-// review_window_overlay.addEventListener("click", hideReview);
-// review_window.addEventListener("click", (event) =&gt; event.stopPropagation());
-
-let container = document.getElementById("all-music");
-let children = container.children;
+let musiccontainer = document.getElementById("all-music");
+let musicchildren = musiccontainer.children;
 
 
 
-let index = Math.floor(Math.random() * children.length);
+let musicindex = Math.floor(Math.random() * musicchildren.length);
 
-for(let i = 0; i < children.length; i++) {
-    if(index != i) {
-        children[i].style.display= "none";
+for(let i = 0; i < musicchildren.length; i++) {
+    if(musicindex != i) {
+        musicchildren[i].style.display= "none";
+    }
+}
+
+let piccontainer = document.getElementById("all-pic");
+let picchildren = piccontainer.children;
+
+
+
+let picindex = Math.floor(Math.random() * picchildren.length);
+
+for(let i = 0; i < picchildren.length; i++) {
+    if(picindex != i) {
+        picchildren[i].style.display= "none";
+    }
+}
+
+
+let reviewcontainer = document.getElementById("all-review");
+let reviewchildren = reviewcontainer.children;
+
+
+
+let reviewindex = Math.floor(Math.random() * reviewchildren.length);
+
+for(let i = 0; i < reviewchildren.length; i++) {
+    if(reviewindex != i) {
+        reviewchildren[i].style.display= "none";
     }
 }
